360b15b58c15d0c8babc9bf276f8239e6d2d42b8
Hibou42 <hibou42@protonmail.com>
2024-04-06 15:07:41 +0200

    start gameplay

---
+++ lib/gameplay/AGameplay.hpp
@@ -0,0 +1,20 @@
+#ifndef AGAMEPLAY_HPP
+# define AGAMEPLAY_HPP
+
+#include <string>
+
+using namespace std;
+
+class AGameplay
+{
+private:
+
+protected :
+
+public:
+	AGameplay();
+	virtual ~AGameplay();
+
+};
+
+#endif
---
+++ lib/gameplay/Easy.hpp
@@ -0,0 +1,18 @@
+#ifndef EASY_HPP
+# define EASY_HPP
+
+#include <string>
+
+using namespace std;
+
+class Easy : public AGameplay
+{
+	private:
+
+	public:
+		Easy();
+		~Easy();
+
+};
+
+#endif
